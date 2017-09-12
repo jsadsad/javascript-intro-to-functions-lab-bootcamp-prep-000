d23ec83c235f6f6a5cf2bdd688e1b531b612a44f
JCS <joshsad2@gmail.com>
2017-09-12 03:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return 'I can\'t hear you!';
-  } else if (string.toUpperCase === string) {
+  } else if (string.toUpperCase() === string) {
     return 'YES INDEED!';
   } else if (string === 'I love you, Grandma.') {
     var message3 = 'I love you, too';
