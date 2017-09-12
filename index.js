f242aed906d7454f4dee1e3e8ff05f0f454b6d58
JCS <joshsad2@gmail.com>
2017-09-12 03:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 }
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase) {
+    console.log('I can\'t hear you!');
+  } else if (string === string.toUpperCase) {
+    console.log('YES INDEED!');
+  } else if (string === 'I love you, Grandma.') {
+    console.log('I love you, too');
+  }
+}
